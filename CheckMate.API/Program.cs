7b8931bede2fb5c3c22771ac22101c208e39b311
JupiterRoue45 <idrisbenarbia1@gmail.com>
2026-09-17 12:49:52 +0200

    [add] added the connection string, dbcontext with datasets and the services injections.

--- CheckMate.API/Program.cs
+++ CheckMate.API/Program.cs
@@ -1,6 +1,21 @@
+using CheckMate.Infrastructure.Identity;
+using CheckMate.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("CheckMate") ??
+        throw new InvalidOperationException("Missing connection string for CheckMate");
+
+
 // Add services to the container.
+builder.Services.AddDbContext<CheckMateDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.
+    AddIdentityCore<User>()
+    .AddRoles<Role>()
+    .AddEntityFrameworkStores<CheckMateDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
---
+++ CheckMate.Infrastructure/Persistence/CheckMateDbContext.cs
@@ -0,0 +1,48 @@
+﻿using CheckMate.Domain.Entities;
+using CheckMate.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace CheckMate.Infrastructure.Persistence
+{
+    public class CheckMateDbContext : IdentityDbContext<User, Role, string>
+    {
+        // DbSets
+        public DbSet<Address> Addresses => Set<Address>();
+        public DbSet<Client> Clients => Set<Client>();
+        public DbSet<Company> Companies => Set<Company>();
+        public DbSet<Country> Countries => Set<Country>();
+        public DbSet<DailyService> DailyServices => Set<DailyService>();
+        public DbSet<Invoice> Invoices => Set<Invoice>();
+        public DbSet<InvoiceRow> InvoiceRows => Set<InvoiceRow>();
+        public DbSet<InvoiceRowHistory> InvoiceRowHistories => Set<InvoiceRowHistory>();
+        public DbSet<Occupant> Occupants => Set<Occupant>();
+        public DbSet<OngoingInvoice> OngoingInvoices => Set<OngoingInvoice>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<PaymentMethod> PaymentMethods => Set<PaymentMethod>();
+        public DbSet<Person> Persons => Set<Person>();
+        public DbSet<Reservation> Reservations => Set<Reservation>();
+        public DbSet<ReservationRoom> ReservationRooms => Set<ReservationRoom>();
+        public DbSet<ReservationType> ReservationTypes => Set<ReservationType>();
+        public DbSet<Room > Rooms => Set<Room>();
+        public DbSet<RoomType> RoomTypes => Set<RoomType>();
+        public DbSet<RoomUnavailability> RoomUnavailabilities => Set<RoomUnavailability>();
+        public DbSet<Service> Services => Set<Service>();
+
+        public CheckMateDbContext(DbContextOptions<CheckMateDbContext> options) : base(options) {
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(CheckMateDbContext).Assembly);
+        }
+    }
+}
